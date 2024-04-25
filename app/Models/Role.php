cef68cccb634ed41f2763f79b4b4cf48f7e27de7
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-04-25 21:27:24 +0200

    update

---
+++ app/Models/Role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'status',
+        'is_active',
+        'created_by',
+        'updated_by',
+    ];
+
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'role_id');
+    }
+
+}
