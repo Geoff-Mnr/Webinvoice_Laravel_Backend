b9c137df254052f91a634091a4ba891a629b2a83
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-04-25 20:58:21 +0200

    Create 2024_04_25_185553_create_document_product_table.php

---
+++ database/migrations/2024_04_25_185553_create_document_product_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_product', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('document_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+            $table->integer('quantity')->default(0);
+            $table->integer('discount')->default(0);
+            $table->integer('price_htva')->default(0);
+            $table->integer('price_vvac')->default(0);
+            $table->integer('price_total')->default(0);
+            $table->longText('comment')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('status', 1)->default('N');
+            $table->boolean('is_active')->default(true);
+            $table->unsignedInteger('created_by')->nullable(false)->default(1);
+            $table->unsignedInteger('updated_by')->nullable(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_product');
+    }
+};
