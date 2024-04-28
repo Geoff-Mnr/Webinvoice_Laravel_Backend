932ddfea89edb0f83f6cd6dbfcef4455d40e1ace
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-04-28 14:09:17 +0200

    add UpdatedByCast

---
+++ app/Casts/UpdatedByCast.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use App\Models\User;
+
+
+class CreatedByCast implements CastsAttributes
+{
+    /**
+     * Cast the value to a type.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function get($model, string $key, $value, array $attributes)
+    {
+        return User::find($value);
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        return $value->id;
+    }
+}
