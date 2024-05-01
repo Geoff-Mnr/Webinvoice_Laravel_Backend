72378fcf2cb4c36d1b8cdece24c29d401c066315
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-05-01 11:38:35 +0200

    Update BaseController.php

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,103 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    /**
+     * Handle the response
+     *
+     * @param $data
+     * @param $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handleResponse($data, $message, $code = 200)
+    {
+        return response()->json([
+            'data' => $data,
+            'message' => $message,
+            'code' => $code,
+            'meta' => [
+                'total' => $data->total(),
+                'per_page' => $data->perPage(),
+                'current_page' => $data->currentPage(),
+                'last_page' => $data->lastPage(),
+                'from' => $data->firstItem(),
+                'to' => $data->lastItem()
+            ],
+            'links' => [
+                'first' => $data->url(1),
+                'last' => $data->url($data->lastPage()),
+                'prev' => $data->previousPageUrl(),
+                'next' => $data->nextPageUrl()
+            ],
+        ], $code);
+    }
+
+    /**
+     * Handle the error response
+     *
+     * @param $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+
+    public function handleErrorResponse($message, $code = 404)
+    {
+        return response()->json([
+            'message' => $message,
+            'code' => $code
+        ], $code);
+    }
+
+    /**
+     * Handle the success response
+     *
+     * @param $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+  
+    public function handleSuccessResponse($message, $code = 200)
+    {
+        return response()->json([
+            'message' => $message,
+            'code' => $code
+        ], $code);
+    }
+
+    /**
+     * Handle the response without pagination
+     *
+     * @param $data
+     * @param $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handleResponseNoPagination($data, $message, $code = 200)
+    {
+        return response()->json([
+            'data' => $data,
+            'message' => $message,
+            'code' => $code,
+        ], $code);
+    }
+
+    /**
+     * Handle the error response
+     *
+     * @param $message
+     * @param int $code
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handleError($message, $code = 404)
+    {
+        return response()->json([
+            'message' => $message,
+            'code' => $code
+        ], $code);
+    }
+}
