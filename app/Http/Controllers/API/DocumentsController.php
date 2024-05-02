13105441ecafa6c71d662f839e9c1b037b4f2bdd
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-05-02 10:44:35 +0200

    Update DocumentsController.php

--- app/Http/Controllers/API/DocumentsController.php
+++ app/Http/Controllers/API/DocumentsController.php
@@ -11,9 +11,31 @@ class DocumentsController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $search = $request->query('search');
+        $perPage = $request->query('perPage', 10);
+
+        try {
+            $query = Document::where('user_id', auth()->user()->id)
+                ->where(function($query) use ($search) {
+                    $query->where('reference_number', 'like', "%$search%")
+                        ->orWhere('document_date', 'like', "%$search%")
+                        ->orWhere('due_date', 'like', "%$search%")
+                        ->orWhere('price_htva', 'like', "%$search%")
+                        ->orWhere('price_vvac', 'like', "%$search%")
+                        ->orWhere('price_total', 'like', "%$search%")
+                        ->orWhere('status', 'like', "%$search%")
+                        ->orWhereHas('documenttypes', function($query) use ($search) {
+                            $query->where('name', 'like', "%$search%");
+                        });
+                });
+
+            $documents = $query->paginate($perPage)->withQueryString();
+            return $this->handleResponse(200, 'Documents fetched successfully', $documents);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
 
     /**
@@ -21,7 +43,23 @@ class DocumentsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'customer_id' => 'required',
+                'documenttype_id' => 'required',
+                'reference_number' => 'required',
+                'document_date' => 'required',
+                'due_date' => 'required',
+                'price_htva' => 'required',
+                'price_vvac' => 'required',
+                'price_total' => 'required',
+            ]);
+
+            $document = Document::create($request->all());
+            return $this->handleResponse(200, 'Document created successfully', $document, 200);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
 
     /**
@@ -29,7 +67,11 @@ class DocumentsController extends Controller
      */
     public function show(Document $document)
     {
-        //
+        try {
+            return $this->handleResponse(200, 'Document fetched successfully', $document, 200);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
 
     /**
@@ -37,7 +79,12 @@ class DocumentsController extends Controller
      */
     public function update(Request $request, Document $document)
     {
-        //
+        try {
+            $document->update($request->all());
+            return $this->handleResponse(200, 'Document updated successfully', $document, 200);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
 
     /**
@@ -45,6 +92,11 @@ class DocumentsController extends Controller
      */
     public function destroy(Document $document)
     {
-        //
+        try {
+            $document->delete();
+            return $this->handleResponse(200, 'Document deleted successfully', null, 200);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }   
     }
 }
