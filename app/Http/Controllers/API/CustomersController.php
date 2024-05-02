5ef812bcb7470eb783544a60214b007a048a5abc
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-05-02 10:37:07 +0200

    Update CustomersController.php

--- app/Http/Controllers/API/CustomersController.php
+++ app/Http/Controllers/API/CustomersController.php
@@ -4,24 +4,62 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Customer;
+use App\Models\Product;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController;
 
-class CustomersController extends Controller
+class CustomersController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+
+    public function index(Request $request)
     {
-        //
+        $search = $request->query('search');
+        $perPage = $request->query('perPage', 10);
+
+        try {
+            $query = Customer::where('user_id', auth()->user()->id)
+                ->where(function($query) use ($search) {
+                    $query->where('name', 'like', "%$search%")
+                        ->orWhere('brand', 'like', "%$search%")
+                        ->orWhere('ean_code', 'like', "%$search%")
+                        ->orWhereHas('products', function($query) use ($search) {
+                            $query->where('name', 'like', "%$search%");
+                        });
+                });
+
+            $customers = $query->paginate($perPage)->withQueryString();
+            return $this->handleResponse(200, 'Customers fetched successfully', $customers);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'name' => 'required',
+                'brand' => 'required',
+                'ean_code' => 'required',
+                'stock' => 'required',
+                'buying_price' => 'required',
+                'selling_price' => 'required',
+                'discount' => 'required',
+            ]);
+
+            $customer = Customer::create($request->all());
+            return $this->handleResponse(200, 'Customer created successfully', $customer);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
 
     /**
@@ -29,7 +67,15 @@ class CustomersController extends Controller
      */
     public function show(Customer $customer)
     {
-        //
+        try {
+            if ($customer->user_id == auth()->user()->id) {
+                return $this->handleResponse(200, 'Customer retrieved successfully', $customer);
+            } else {
+                return $this->handleError('Customer not found', 400);
+            }
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
 
     /**
@@ -37,7 +83,12 @@ class CustomersController extends Controller
      */
     public function update(Request $request, Customer $customer)
     {
-        //
+        try {
+        $customer->update($request->all());
+        return $this->handleResponse(200, 'Customer updated successfully', $customer);
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
 
     /**
@@ -45,6 +96,15 @@ class CustomersController extends Controller
      */
     public function destroy(Customer $customer)
     {
-        //
+        try {
+            if ($customer->user_id == auth()->user()->id) {
+                $customer->delete();
+                return $this->handleResponse(200, 'Customer deleted successfully', $customer);
+            } else {
+                return $this->handleError('Customer not found', 400);
+            } 
+        } catch (\Exception $e) {
+            return $this->handleError($e->getMessage(),400);
+        }
     }
 }
