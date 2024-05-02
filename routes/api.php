3db4cdfee96252f1976c9ae11aaf83a6c36d143d
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-05-02 15:47:21 +0200

    update

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,6 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
 Route::post('/login', [\App\Http\Controllers\Auth\LoginController::class, 'login']);
 Route::post('/register', [\App\Http\Controllers\Auth\RegisterController::class, 'register']);
 
