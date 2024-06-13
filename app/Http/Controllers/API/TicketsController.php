63ab2567907602dae7dccbed182b7157b4c2ce8a
Geoff Mnr <97231176+Geoff-Mnr@users.noreply.github.com>
2024-06-13 06:37:32 +0200

    Update TicketsController.php

--- app/Http/Controllers/API/TicketsController.php
+++ app/Http/Controllers/API/TicketsController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Tickets;
+use App\Models\User;
+use App\Http\Resources\TicketResource;
 use Illuminate\Http\Request;
 
-class TicketsController extends Controller
+class TicketsController extends BaseController
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $tickets = Tickets::all()->orderBy('created_at', 'desc')->get();
+        return $this->handleResponseNoPagination(TicketResource::collection($tickets), 'Tickets retrieved successfully',200);
     }
+
 
     /**
      * Store a newly created resource in storage.
